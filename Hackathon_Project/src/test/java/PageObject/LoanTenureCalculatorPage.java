824d9df9c01b36ae2fbf991daed5664d1b1321d9
Apoorva <krishanapoorv1@gmail.com>
2024-02-13 11:56:55 +0530

    this is my first commit

---
+++ Hackathon_Project/src/test/java/PageObject/LoanTenureCalculatorPage.java
@@ -0,0 +1,12 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoanTenureCalculatorPage extends BasePage {
+
+	public LoanTenureCalculatorPage(WebDriver driver) {
+		super(driver);
+
+	}
+
+}
